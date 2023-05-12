f377e40d06087b541353c47b2a1ca1a2e1729ecf
Trezy <t@trezy.com>
2023-05-12 12:19:38 -0500

    add support for broadcasting like events

--- packages/lib/src/index.js
+++ packages/lib/src/index.js
@@ -5,8 +5,15 @@ export { SessionData } from './types/SessionData.js'
 
 // Structures
 export { API } from './API.js'
-export { User } from './User.js'
+export { AppBskyFeedLikeEvent } from './AppBskyFeedLikeEvent.js'
+export { EventEmitter } from './EventEmitter.js'
+export { Firehose } from './Firehose.js'
+export { FirehoseMessage } from './FirehoseMessage.js'
+export { FirehoseMessageOperation } from './FirehoseMessageOperation.js'
 export { Skeet } from './Skeet.js'
+export { User } from './User.js'
 
 // Helpers
 export { parseATURL } from './helpers/parseATURL.js'
+export * from './helpers/defaults.js'
+export * from './helpers/eventNames.js'
